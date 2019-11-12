438115c6944965e1be19bbb43abb8371406a28a3
Saket Roy <roy05@live.missouristate.edu>
2019-11-12 20:20:56 +0530

    Updates

--- precache-manifest.af6bfdadef74fd481de06f459509c424.js
+++ precache-manifest.af6bfdadef74fd481de06f459509c424.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0f98ca93889e26d45828166bd9c3863b",
+    "revision": "8c87bafa2c80c3151cdbb59755e709dc",
     "url": "/sort-visualizer/index.html"
   },
   {
-    "revision": "7cf3f53a2cb4235c7565",
-    "url": "/sort-visualizer/static/css/main.86dce605.chunk.css"
+    "revision": "d81d7c3fb01d23fafed4",
+    "url": "/sort-visualizer/static/css/main.d3db3741.chunk.css"
   },
   {
     "revision": "a6874f5bb102b2d39f1f",
     "url": "/sort-visualizer/static/js/2.44241eb3.chunk.js"
   },
   {
-    "revision": "7cf3f53a2cb4235c7565",
-    "url": "/sort-visualizer/static/js/main.7cc1e4eb.chunk.js"
+    "revision": "d81d7c3fb01d23fafed4",
+    "url": "/sort-visualizer/static/js/main.96190b66.chunk.js"
   },
   {
     "revision": "b909a94fd1328b33c9c7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sort-visualizer/precache-manifest.1dc6b88963ae958fdaa8161fd574de72.js"
+  "/sort-visualizer/precache-manifest.af6bfdadef74fd481de06f459509c424.js"
 );
 
 self.addEventListener('message', (event) => {
