a3aa961a1ceb4cf764916e2e4f6bbbed2f09d070
Saket Roy <saket.roy@hotmail.com>
2019-11-24 19:10:44 +0530

    Heap Sort Visualized

--- src/SortingVisualizer/SortingVisualizer.js
+++ src/SortingVisualizer/SortingVisualizer.js
@@ -653,22 +653,30 @@ class SortingVisualizer extends React.Component {
 
     
     //HEAPSORT ANIMATION FUNCTION(S)
-    heapSort(arr){
-        let n = arr.length;
+    heapSort(){
+        
+        this.setState({startedSort: true});
 
+        const arr = [...this.state.array],
+            n = arr.length,
+            animations = [];
+
+        //Create initial max heap
         for(let i = Math.floor(n/2)-1; i>=0;i--){
-            this.heapify(arr, n, i);
+            this.heapify(arr, n, i, animations);
         }
 
+        //swap root node with last element
         for(let j = n-1; j>=0; j--){
             this.swap(arr, j, 0);
-            this.heapify(arr, j, 0);
+            animations.push([j,0]);
+            this.heapify(arr, j, 0, animations);
         }
 
-        console.log(arr)
+        this.animateHeapSort(animations);
     }
 
-    heapify(arr, n, i){
+    heapify(arr, n, i, animations){
         let largest = i,
             left = 2*i+1,
             right = 2*i+2;
@@ -682,12 +690,41 @@ class SortingVisualizer extends React.Component {
         }
 
         if(largest !== i){
-            console.log(arr);
             this.swap(arr, i, largest);
-            console.log(arr);
-            this.heapify(arr, n, largest);
+            animations.push([i,largest]);
+            this.heapify(arr, n, largest, animations);
         }
 
+    }
+
+    animateHeapSort(animations){
+        const arr = this.state.array,
+            array_bar = document.getElementsByClassName("array-elem"),
+            TIME = this.state.TIME,
+            hMult = this.state.heightMultiplier;
+        
+        for(let i=0; i< animations.length; i++){
+            setTimeout(()=>{
+                let elem1 = animations[i][0],
+                elem2 = animations[i][1];
+
+                array_bar[elem1].style.backgroundColor = `red`;
+                array_bar[elem2].style.backgroundColor = `blue`;
+
+                setTimeout(()=>{
+                    this.swap(arr, elem1, elem2);
+                    array_bar[elem1].style.height = `${hMult*arr[elem1]}px`;
+                    array_bar[elem2].style.height = `${hMult*arr[elem2]}px`;
+                }, TIME/20);
+                
+                setTimeout(()=>{
+                    array_bar[elem1].style.backgroundColor = `lightblue`;
+                    array_bar[elem2].style.backgroundColor = `lightblue`;
+                }, TIME/10 - 10);
+            }, i*TIME/10);
+        }
+    
+        this.sortCompleteAnimation(animations.length/10);
     }
 
     //END OF HEAPSORT ANIMATION FUNCTION(S)
@@ -711,7 +748,7 @@ class SortingVisualizer extends React.Component {
                     <button className="nav-btn" id = "quick-sort" onClick = {()=>{this.quickSort()}}>Quick Sort</button>
                     <button className="nav-btn" id = "merge-sort" onClick = {()=>{this.mergeSort()}}>Merge Sort</button>
                     <button className="nav-btn" id = "bead-sort" onClick = {()=>{this.beadSort()}}>Bead Sort</button>
-                    <button className="nav-btn" id = "bead-sort" onClick = {()=>{this.heapSort([1,5,2,4,3])}}>Heap Sort</button>
+                    <button className="nav-btn" id = "bead-sort" onClick = {()=>{this.heapSort()}}>Heap Sort</button>
                     <button className="nav-btn" id = "test-algs" onClick = {()=>{this.testAlgorithms()}}>Test!</button>
                 </nav>
                 <div className="array-container">
