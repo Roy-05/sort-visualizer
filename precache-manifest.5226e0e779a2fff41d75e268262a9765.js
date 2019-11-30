c3c72c5caaf04d854583bd59a107b7e882f8b106
Saket Roy <saket.roy@hotmail.com>
2019-11-30 11:54:11 +0530

    Updates

--- precache-manifest.5226e0e779a2fff41d75e268262a9765.js
+++ precache-manifest.5226e0e779a2fff41d75e268262a9765.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bd7765ac89d0c0b5fd663de29687a373",
+    "revision": "b87ec6167b9bb1dc0c87ab27c9113faa",
     "url": "/sort-visualizer/index.html"
   },
   {
-    "revision": "484a852879f6ee1321fa",
-    "url": "/sort-visualizer/static/css/main.0ab8ad76.chunk.css"
+    "revision": "5e07b8923b01ccba6218",
+    "url": "/sort-visualizer/static/css/main.92337c2d.chunk.css"
   },
   {
     "revision": "20c0d4e32d1a75db7beb",
     "url": "/sort-visualizer/static/js/2.2faf8e9b.chunk.js"
   },
   {
-    "revision": "484a852879f6ee1321fa",
-    "url": "/sort-visualizer/static/js/main.62186f6d.chunk.js"
+    "revision": "5e07b8923b01ccba6218",
+    "url": "/sort-visualizer/static/js/main.98f34649.chunk.js"
   },
   {
     "revision": "b909a94fd1328b33c9c7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sort-visualizer/precache-manifest.d45bdd19281518640d3b1e99199be7d6.js"
+  "/sort-visualizer/precache-manifest.5226e0e779a2fff41d75e268262a9765.js"
 );
 
 self.addEventListener('message', (event) => {
